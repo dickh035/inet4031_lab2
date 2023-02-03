c6a79e9f22785d02be97289dbb7d95908a1bb49d
dickh035 <dickh035@umn>
2023-02-03 20:10:38 +0000

    missing py

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@
 a = 2
 b = 2
 c = a + b
-
+listOfUsers = ["User1", "User2", "User3"]
 print ("Python says: Hello, World!")
 print ("%s + %s = %s" % (a,b,c))
+for i in range(len(listOfUsers)):
+ print(listOfUsers[i])
